490f18999710a54e6c907bc5aa5fa17340796e4f
Almir Ishmukhametov <alimfyl@gmail.com>
2026-06-16 22:12:58 +0500

    +calendar

---
+++ skypro-wallet/src/components/Calendar/Calendar.styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+export const Wrapper = styled.div`
+background: white;
+padding: 30px;
+border-radius: 20px;
+`;
+export const Title = styled.h2`
+margin-bottom: 25px;
+font-size: 24px;
+`;
+export const Grid = styled.div`
+display: grid;
+grid-template-columns: repeat(7, 1fr);
+gap: 10px;
+`;
+export const Day = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+height: 42px;
+border-radius: 10px;
+background: #F5F5F7;
+font-weight: 500;
+`;
