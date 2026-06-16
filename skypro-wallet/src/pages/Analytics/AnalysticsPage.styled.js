7edb3ca89af9b8ecada59258802f655b63a12ab1
Almir Ishmukhametov <alimfyl@gmail.com>
2026-06-16 21:58:06 +0500

    +AnalysticsPage

---
+++ skypro-wallet/src/pages/Analytics/AnalysticsPage.styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+min-height: 100vh;
+`;
+export const Container = styled.div`
+padding: 40px;
+display: flex;
+gap: 30px;
+align-items: flex-start;
+`;
+export const Left = styled.div`
+flex: 1;
+`;
+export const Right = styled.div`
+flex: 2;
+`;
--- skypro-wallet/src/pages/Analytics/AnalyticsPage.jsx
+++ skypro-wallet/src/pages/Analytics/AnalyticsPage.jsx
@@ -1,50 +1,21 @@
-import { useState, useEffect } from "react";
-import { Outlet } from "react-router-dom";
-import { fetchTasks } from "../../api/tasks";
-import Header from "../../components/Header/Header";
-import Main from "../../components/Main/Main";
+import Header from '../../components/Header/Header';
+import Calendar from '../../components/Calendar/Calendar';
+import Chart from '../../components/Chart/Chart';
+import { Wrapper, Container, Left, Right } from './AnalysticsPage.styled';
 
-function MainPage() {
-  const [cards, setCards] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  const refreshTasks = () => {
-    fetchTasks ()
-    .then((data) => {
-      setCards(data);
-    })
-    .catch((err) => {
-      setError(err.message);
-    })
-    .finally(() => {
-      setIsLoading(false);
-    });
-  }
-
-  useEffect (() =>{
-  refreshTasks();
-}, [] );
+const AnalyticsPage = () => {
   return (
-    <div className="wrapper">
-        
-      <Outlet context={{ cards, refreshTasks }} /> 
-
-      <Header />
-
-      {error && (
-        <p style={{color:"red", textAlign: "center", marginTop: "20px"}}>
-          {error}
-        </p>
-      )}
-
-      {isLoading ? (
-        <p style={{ textAlign: "center", marginTop: "50px" }}>Данные загружаются...</p>
-      ) : (
-        <Main cards={cards} />
-      )}
-    </div>
+    <Wrapper>
+      <Container>
+        <Left>
+          <Calendar />
+        </Left>
+        <Right>
+          <Chart />
+        </Right>
+      </Container>
+    </Wrapper>
   );
-}
+};
 
-export default MainPage;
+export default AnalyticsPage;
